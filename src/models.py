e262bd4418425456a073736dce127c83f18256ee
perd007 <95108549+perd007@users.noreply.github.com>
2023-08-14 23:51:23 +0000

    Modificando favorities

--- src/models.py
+++ src/models.py
@@ -11,11 +11,11 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=True)
+    name = Column(String(50), nullable=False)
     last_name=Column(String(50), nullable=False)
-    email= Column(String(50), nullable=True)
-    password= Column(String(20), nullable=True)
-    date_sus=Column(String(20),nullable=True)
+    email= Column(String(50), nullable=False)
+    password= Column(String(20), nullable=False)
+    date_sus=Column(String(20),nullable=False)
     favorites= relationship("Favorities", backref="user")
 
 
@@ -39,14 +39,14 @@ class Planet(Base):
 class People(Base):
     __tablename__='people'
     id=Column(Integer, primary_key=True)
-    name= Column(String(50),nullable=True)
-    height= Column(Integer,nullable=True)
+    name= Column(String(50),nullable=False)
+    height= Column(Integer,nullable=False)
     mass= Column(Integer,nullable=True)
     hair_color= Column(String(50),nullable=True)
     skin_color= Column(String(50),nullable=True)
-    eye_color= Column(String(50),nullable=True)
-    birth_year= Column(String(50),nullable=True)
-    gender= Column(String(50),nullable=True)
+    eye_color= Column(String(50),nullable=False)
+    birth_year= Column(String(50),nullable=False)
+    gender= Column(String(50),nullable=False)
     planet_id= Column(Integer, ForeignKey("planet.id"), nullable=True)
     favorities= relationship("Favorites", backref="poeple")
   
@@ -54,9 +54,9 @@ class People(Base):
 class Favorites(Base):
     __tablename__='favorities'
     id= Column(Integer, primary_key=True)
-    user_id= Column(Integer, ForeignKey("user.id"), nullable=True)
-    planet_id= Column(Integer,ForeignKey("planet.id"), nullable=False)
-    people_id= Column(Integer, ForeignKey("people.id"),nullable=False)
+    user_id= Column(Integer, ForeignKey("user.id"), nullable=False)
+    planet_id= Column(Integer,ForeignKey("planet.id"), nullable=True)
+    people_id= Column(Integer, ForeignKey("people.id"),nullable=True)
 
     def to_dict(self):
         return {}
