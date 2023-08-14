a2451ef9421a1110d39c3435a6c29c1a4666b534
perd007 <95108549+perd007@users.noreply.github.com>
2023-08-14 21:43:05 +0000

    diagramando con python

--- src/models.py
+++ src/models.py
@@ -7,23 +7,56 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=True)
+    last_name=Column(String(50), nullable=False)
+    email= Column(String(50), nullable=True)
+    password= Column(String(20), nullable=True)
+    date_sus=Column(String(20),nullable=True)
+    favorites= relationship("Favorities", backref="user")
+
+
+class Planet(Base):
+    __tablename__='planet'
+    id=Column(Integer, primary_key=True)
+    name= Column(String(50),nullable=True)
+    population= Column(Integer, nullable=True)
+    rotation_period= Column(Integer,nullable=False)
+    orbital_period= Column(Integer,nullable=False)
+    diameter= Column(Integer,nullable=False)
+    climate= Column(String(50),nullable=False)
+    gravity= Column(String(50),nullable=False)
+    terrain= Column(String(50),nullable=False)
+    surface_water= Column(Integer,nullable=False)
+    people_id= relationship("People", backref="planet")
+    favorities= relationship("Favorities", backref="planet")
+
+
+
+class People(Base):
+    __tablename__='people'
+    id=Column(Integer, primary_key=True)
+    name= Column(String(50),nullable=True)
+    height= Column(Integer,nullable=True)
+    mass= Column(Integer,nullable=True)
+    hair_color= Column(String(50),nullable=True)
+    skin_color= Column(String(50),nullable=True)
+    eye_color= Column(String(50),nullable=True)
+    birth_year= Column(String(50),nullable=True)
+    gender= Column(String(50),nullable=True)
+    planet_id= Column(Integer, ForeignKey("planet.id"), nullable=True)
+    favorities= relationship("Favorites", backref="poeple")
+  
+    
+class Favorites(Base):
+    __tablename__='favorities'
+    id= Column(Integer, primary_key=True)
+    user_id= Column(Integer, ForeignKey("user.id"), nullable=True)
+    planet_id= Column(Integer,ForeignKey("planet.id"), nullable=False)
+    people_id= Column(Integer, ForeignKey("people.id"),nullable=False)
 
     def to_dict(self):
         return {}
